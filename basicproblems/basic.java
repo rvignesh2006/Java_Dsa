d90570ec9d73e68291ef902bf4672bbece0846c9
Vignesh R <rvignesh20006@gmail.com>
2026-07-23 19:42:51 +0530

    Day 1: Add Java basic problems

---
+++ basicproblems/basic.java
@@ -0,0 +1,24 @@
+package basicproblems;
+import java.util.*;
+
+public class basic {
+    public static  boolean isEven(int n){
+        int rem = n%2;
+        if(rem==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String args[]){
+            int n =18;
+            if(isEven(n)){
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
+    }
+    
+}
